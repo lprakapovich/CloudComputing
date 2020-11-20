8d52a7235dbf235aae60aff3f3439596b4f0c838
Liza <audlisab@gmail.com>
2020-11-20 15:34:53 +0100

    chatroom component + Angular material configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
-
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AmplifyUIAngularModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
